2c9dcaa3eb9d3ce7cb80c13eb2a7f745f0b1c937
Ana Luisa <anadrvv@gmail.com>
2026-05-01 14:57:07 -0300

    configurando projeto e primeiros testes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
+import "./App.css";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
- 
-
   return (
     <>
-      
+      <AppRoutes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
