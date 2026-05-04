a32c90ea1fed4f30b5387fcc5b95d381ac427de2
lavi <165945697+scriptlver@users.noreply.github.com>
2026-05-04 19:52:30 -0300

    come over - comecei e fiz algumas coisas

--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -1,6 +1,28 @@
+import Layout from "../../Layout/Layout";
+
 function Home() {
   return (
-    <h1 className="text-2xl bg-white">teste</h1>
+    <Layout>
+      <div className="px-6">
+        <div className="flex gap-16">
+          <div className="flex-1">
+            <header className="py-6 text-text font-semibold flex items-center justify-between">
+              <h1 className="text-2xl font-semibold">Cursos</h1>
+              <div className="flex items-center gap-2 text-sm font-normal">
+                <span>Filtrar por:</span>
+                <select className="border border-gray-300 rounded-md px-3 py-1 text-sm bg-white text-gray-700 cursor-pointer">
+                  <option>Matriz</option>
+                </select>
+              </div>
+            </header>
+            <div className="bg-background-white rounded-lg p-6 flex flex-col gap-4 w-full h-[500px]"></div>
+          </div>
+          <aside className="w-64 py-6 mt-13">
+            <h2 className="text-text font-semibold text-lg">Notificações</h2>
+          </aside>
+        </div>
+      </div>
+    </Layout>
   );
 }
 
