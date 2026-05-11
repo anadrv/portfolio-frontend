c15bc191c75faa36a74d4f9ec139d66419435b41
Ana Luisa <anadrvv@gmail.com>
2026-05-11 20:07:49 -0300

    Ajustes de tela

--- portfolio/src/Layout/Layout.jsx
+++ portfolio/src/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import Menu from "../components/Menu";
 
 function Layout({ children }) {
   return (
-    <div className="relative min-h-screen w-full bg-background px-4 py-4 md:px-10 lg:px-20 xl:px-32 2xl:px-36 md:pt-12 3xl:px-72">
+    <div className="relative min-h-screen w-full bg-background px-4 py-4 md:px-10 lg:px-20 md:pt-12 3xl:px-72">
       <div
         className="absolute inset-0 bg-cover bg-no-repeat"
         style={{
