f3889d3535adbaa5946ab028e0d7316b2091ddaa
Sabrina Vitoria <sabrinavitoria24@gmail.com>
2026-05-14 06:03:00 -0300

    arrumando

--- portfolio/src/pages/Login/Login.jsx
+++ portfolio/src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import LoginLayout from "../../Layout/LoginLayout";
+import bgImage from "../../assets/images/bg-image.png";
 import unifacisa from "../../assets/images/name-image.png";
 import logo from "../../assets/icons/unifacisa-icon.png";
 import border from "../../assets/images/border-image.png";
@@ -46,147 +46,157 @@ function Login() {
   }
 
   return (
-    <LoginLayout>
+    <div className="relative min-h-screen w-full bg-sky-200 overflow-hidden">
 
       <img
-        src={unifacisa}
-        alt="Unifacisa"
-        className="hidden md:block absolute top-13 left-20 w-56 z-20"
+        src={bgImage}
+        alt="background"
+        className="absolute top-20 left-0 w-full h-[89%]"
       />
-      <div className="min-h-screen flex items-center justify-center px-0 md:px-6">
 
-        <div className="bg-blue-800 w-full max-w-3xl rounded-none md:rounded-3xl shadow-2xl p-6 md:p-12 flex flex-col md:flex-row min-h-screen md:min-h-0 md:mt-5">
+      <div className="relative z-10">
 
-          <div className="hidden md:flex w-1/2 items-center justify-center relative">
+        <img
+          src={unifacisa}
+          alt="Unifacisa"
+          className="hidden md:block absolute top-13 left-20 w-56 z-20"
+        />
 
-            <img
-              src={border}
-              alt=""
-              className="w-52 absolute left-12"
-            />
+        <div className="min-h-screen flex items-center justify-center px-0 md:px-6">
 
-            <img
-              src={bo}
-              alt=""
-              className="w-45 absolute right-8"
-            />
+          <div className="bg-background w-full max-w-3xl rounded-none md:rounded-3xl shadow-2xl p-6 md:p-12 flex flex-col md:flex-row min-h-screen md:min-h-0 md:mt-5">
 
-            <div className="bg-sky-200 border-3 border-slate-400 rounded-md w-70 h-80 flex items-center justify-center absolute z-20 right-13">
+            <div className="hidden md:flex w-1/2 items-center justify-center relative">
 
               <img
-                src={jack}
-                alt="Jack"
-                className="w-56 mt-15"
+                src={border}
+                alt=""
+                className="w-52 absolute left-12"
               />
-
-            </div>
-          </div>
-          
-          <div className="w-full md:w-1/2 flex flex-col justify-center">
-
-            <div className="md:hidden flex mb-10">
 
               <img
-                src={logo}
-                alt="Unifacisa"
-                className="w-12 ml-25"
+                src={bo}
+                alt=""
+                className="w-45 absolute right-8"
               />
 
-              <span className="font-semibold text-3xl text-white font-sans mt-2 ml-2">
-                Unifacisa
-              </span>
+              <div className="bg-sky-200 border-3 border-slate-400 rounded-md w-70 h-80 flex items-center justify-center absolute z-20 right-13">
 
-            </div>
-
-            <h1 className="hidden md:block text-4xl font-bold text-white mb-4">
-              Entrar
-            </h1>
-
-            <form
-              className="w-full flex flex-col gap-4 md:gap-3 ml-8 md:ml-0 mt-10 md:mt-0"
-              onSubmit={handleSubmit}
-            >
-
-              <div>
-
-                <label className="hidden md:block text-sm text-white mb-2">
-                  E-mail:
-                </label>
-
-                <div className="relative">
-
-                  <div className="absolute left-0 top-0 h-full w-3 bg-sky-400 rounded-l-2xl"></div>
-
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    className="w-80 md:w-85 py-3 md:py-2 pl-5 pr-3 rounded-2xl border border-gray-300 outline-none focus:border-blue-400 bg-white text-xl md:text-base md:placeholder-transparent"
-                  />
-
-                </div>
-              </div>
-
-              <div>
-
-                <label className="hidden md:block text-sm text-white mb-2">
-                  Senha:
-                </label>
-
-                <div className="relative">
-
-                  <div className="absolute left-0 top-0 h-full w-3 bg-sky-400 rounded-l-2xl"></div>
-
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Senha"
-                    value={formData.password}
-                    onChange={handleChange}
-                    className="w-80 md:w-85 py-3 md:py-2 pl-5 pr-3 rounded-2xl border border-gray-300 outline-none focus:border-blue-400 bg-white text-xl md:text-base md:placeholder-transparent"
-                  />
-
-                </div>
-              </div>
-
-              <div className="flex items-center gap-2 mt-1">
-
-                <input
-                  type="checkbox"
-                  id="remember"
-                  className="w-4 h-4 accent-lime-300"
+                <img
+                  src={jack}
+                  alt="Jack"
+                  className="w-56 mt-15"
                 />
 
-                <label
-                  htmlFor="remember"
-                  className="text-white text-sm"
-                >
-                  Continuar conectado(a)
-                </label>
+              </div>
+            </div>
+
+            <div className="w-full md:w-1/2 flex flex-col justify-center">
+
+              <div className="md:hidden flex mb-10">
+
+                <img
+                  src={logo}
+                  alt="Unifacisa"
+                  className="w-12 ml-25"
+                />
+
+                <span className="font-semibold text-3xl text-white font-sans mt-2 ml-2">
+                  Unifacisa
+                </span>
 
               </div>
 
-              <button
-                type="submit"
-                className="w-80 md:w-85 bg-lime-400 text-blue-800 text-xl md:text-base font-semibold py-3 md:py-2 rounded-2xl mt-1"
-              >
-                Login
-              </button>
+              <h1 className="hidden md:block text-4xl font-bold text-white mb-4">
+                Entrar
+              </h1>
 
-              <a
-                href="#"
-                className="text-white text-sm ml-23 md:ml-0 md:text-center mt-1 hover:underline"
+              <form
+                className="w-full flex flex-col gap-4 md:gap-3 ml-8 md:ml-0 mt-10 md:mt-0"
+                onSubmit={handleSubmit}
               >
-                Esqueceu a senha?
-              </a>
 
-            </form>
+                <div>
+
+                  <label className="hidden md:block text-sm text-white mb-2">
+                    E-mail:
+                  </label>
+
+                  <div className="relative">
+
+                    <div className="absolute left-0 top-0 h-full w-3 bg-primary rounded-l-lg"></div>
+
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      className="w-80 md:w-85 py-3 md:py-2 pl-5 pr-3 rounded-lg border border-gray-300 outline-none bg-white text-xl md:text-base md:placeholder-transparent"
+                    />
+
+                  </div>
+                </div>
+
+                <div>
+
+                  <label className="hidden md:block text-sm text-white mb-2">
+                    Senha:
+                  </label>
+
+                  <div className="relative">
+
+                    <div className="absolute left-0 top-0 h-full w-3 bg-primary rounded-l-lg"></div>
+
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Senha"
+                      value={formData.password}
+                      onChange={handleChange}
+                      className="w-80 md:w-85 py-3 md:py-2 pl-5 pr-3 rounded-lg border border-gray-300 outline-none bg-white text-xl md:text-base md:placeholder-transparent"
+                    />
+
+                  </div>
+                </div>
+
+                <div className="flex items-center gap-2 mt-1">
+
+                  <input
+                    type="checkbox"
+                    id="remember"
+                    className="w-4 h-4 accent-accent"
+                  />
+
+                  <label
+                    htmlFor="remember"
+                    className="text-white text-sm"
+                  >
+                    Continuar conectado(a)
+                  </label>
+
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-80 md:w-85 bg-accent text-background text-xl md:text-base font-semibold py-3 md:py-2 rounded-lg mt-1"
+                >
+                  Login
+                </button>
+
+                <a
+                  href="#"
+                  className="text-white text-sm ml-23 md:ml-0 md:text-center mt-1 hover:underline"
+                >
+                  Esqueceu a senha?
+                </a>
+
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </LoginLayout>
+    </div>
   );
 }
 
